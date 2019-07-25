1b0da5db20321deb06e6393cce464b0edfcd45ad
sbrunomello <sbrunomello@gmail.com>
2019-07-25 19:56:23 -0300

    user refatorado

--- src/main/java/com/brunomello/webapp/config/Instantiation.java
+++ src/main/java/com/brunomello/webapp/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.brunomello.webapp.domain.Post;
 import com.brunomello.webapp.domain.User;
+import com.brunomello.webapp.dto.AuthorDTO;
 import com.brunomello.webapp.repository.PostRepository;
 import com.brunomello.webapp.repository.UserRepository;
 
@@ -38,9 +39,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green","alex@gmail.com");
 		User bob = new User(null, "Bob Grey","bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Hello World", "Teste de publicação", maria);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Hello World", "Teste de publicação", new AuthorDTO (maria));
+		
 		postRepository.saveAll(Arrays.asList(post1));
 		
 	}
