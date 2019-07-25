2c2f8150fc7b768a087d4c0b4b1ea5736b3d5e93
sbrunomello <sbrunomello@gmail.com>
2019-07-24 23:43:19 -0300

    entity Post

--- src/main/java/com/brunomello/webapp/config/Instantiation.java
+++ src/main/java/com/brunomello/webapp/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.brunomello.webapp.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.brunomello.webapp.domain.Post;
 import com.brunomello.webapp.domain.User;
+import com.brunomello.webapp.repository.PostRepository;
 import com.brunomello.webapp.repository.UserRepository;
 
 
@@ -17,16 +21,27 @@ public class Instantiation implements CommandLineRunner {
 	private UserRepository userRepository;
 	
 	
+	@Autowired
+	private PostRepository postRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown","maria@gmail.com");
 		User alex = new User(null, "Alex Green","alex@gmail.com");
 		User bob = new User(null, "Bob Grey","bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Hello World", "Teste de publicação", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1));
 		
 	}
 
